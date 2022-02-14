44f0a4138742215acf6e1c4cbfbfe76781d89539
Shreya Sharma <shreya_sharma1@baylor.edu>
2022-02-14 15:10:21 -0600

    quiz 1 before time

--- processStudents.py
+++ processStudents.py
@@ -21,30 +21,75 @@ through the logic of the problem.
 
 
 import csv
-
+def main():
 
 # create a file object to open the file in read mode
 
+    infile = open("students.csv",'r')
+
+    students_file = csv.reader(infile, delimiter = ',')
 
 
 # create a csv object from the file object
 
+    students_file = csv.reader(infile, delimiter = ',')
 
 #skip the header row
 
 
 #create an outfile object for the pocessed record
-
+    outfile = open("processedStudents.csv",'w')
 
 
 #create a new dictionary named 'student_dict'
 
-
+    student_dict = {
+    "stud_id":{
+        "firstname":{
+            "lastname":{
+                "major":{
+                    "classification":{
+                        "gpa:"
+                    }
+                }
+            }
+        }
+    }
+}
+    
+    
 
 #use a loop to iterate through each row of the file
+    print(student_dict['stud_id']['firstname']['lastname']['major']['classification']['gpa'])
+
 
     #check if the GPA is below 3.0. If so, write the record to the outfile
+    for record in students_file:
+        StudID = record[0]
+        FirstName = record[2]
+        LastName = [3]
+        Major = [6]
+        Class = [7]
+        Gpa = ([8] < 3.0)
+        outfile.write(StudID + ',' + FirstName + ',' + LastName + ',' Major )
+        
+
+
+
+
+        for record in student_file:
+       FirstName = record[0]
+       Grade1 = record[1]
+       Grade2 = record[2]
+       Grade3 = record[3]
+       avg = (float(record[1]) + float(record[2]) + float(record[3]))/3
+       outfile.write(FirstName + "," + format(round(avg,2)) + "\n") 
     
+    infile.close()
+    outfile.close()
+    
+main()
+
         
 
 
